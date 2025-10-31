cb338369098cfd8a08f5f180071f27f4e61765f1
nganaz <amwenengana@gmail.com>
2025-10-31 11:52:28 +0200

    Third commit

--- app/src/main/java/com/example/myschool/AppHeader.kt
+++ app/src/main/java/com/example/myschool/AppHeader.kt
@@ -16,10 +16,10 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myschool.ui.theme.MySchoolTheme
 
@@ -35,10 +35,10 @@ fun AppHeader(
 
     CenterAlignedTopAppBar(
         colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = Color(0xFFADD8E6),
-            titleContentColor = Color.Black,
-            navigationIconContentColor = Color.Black,
-            actionIconContentColor = Color.Black
+            containerColor = MaterialTheme.colorScheme.primary,
+            titleContentColor = MaterialTheme.colorScheme.onPrimary,
+            navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
+            actionIconContentColor = MaterialTheme.colorScheme.onPrimary
         ),
         title = {
             Text("MySchool")
--- app/src/main/java/com/example/myschool/screens/WelcomeScreen.kt
+++ app/src/main/java/com/example/myschool/screens/WelcomeScreen.kt
@@ -7,13 +7,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,10 +42,6 @@ fun WelcomeScreen(
 
             Button(
                 onClick = onGetStartedClicked,
-                colors = ButtonDefaults.buttonColors(
-                        containerColor = Color.DarkGray,
-                        contentColor = MaterialTheme.colorScheme.onPrimary
-                    )
             ) {
                 Text("Get Started")
             }
